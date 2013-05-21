22faea83de05dc6500e246f39f641c0ac0a437dc
antoinemenini <antoinou2958@gmail.com>
2013-05-21 17:14:32 +0200

    Added data models

--- lib/rmeetup2/request.rb
+++ lib/rmeetup2/request.rb
@@ -1,14 +1,26 @@
 module RMeetup2
 
   class Request
-    
-    def initialize( model)
-      @model = model
-      @params = {:key => RMeetup2::Client.api_key}
+    @@models = {
+      :categories => '2/categories',
+      :checkins => '2/checkins',
+      :cities => '2/cities',
+      :events => '2/events',
+      :groups => '2/groups',
+      :groups3 => 'find/groups',
+      :topics => 'topics',
+      :members => '2/members',
+      :rsvps => '2/rsvps'
+    }
+        
+    def initialize(model, params = {})
+      @model = @@models[model]
+      @params = params
+      @params[:key] = RMeetup2::Client.api_key
     end
 
     def execute
-      response = Nestful.get("https://api.meetup.com/2/#{@model}?key=#{ @params[:key]}")
+      response = Nestful.get("https://api.meetup.com/#{@model}?key=#{ @params[:key]}")
       # http response available in : response.status
       return JSON.parse(response.body)["results"]
     end
